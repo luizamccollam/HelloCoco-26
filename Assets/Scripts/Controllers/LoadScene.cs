668f2625520bb79891902793bbd350ff76f3d866
luizamccollam <106613532+luizamccollam@users.noreply.github.com>
2023-10-03 17:30:36 +0100

    Avatar

---
+++ Assets/Scripts/Controllers/LoadScene.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+  public void LoadSceneUsingName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+  public void ReloadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
